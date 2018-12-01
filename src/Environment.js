b59dd5d33935f887ed38f9741660688d0a4f2a45
Ye Chuah <yechuah@gmail.com>
2018-12-01 21:40:21 +1100

    caching

--- src/Environment.js
+++ src/Environment.js
@@ -3,12 +3,33 @@ const {
   Network,
   RecordSource,
   Store,
+  QueryResponseCache,
 } = require('relay-runtime')
 
 const store = new Store(new RecordSource())
 const __RELAY_API_ENDPOINT__ = 'https://api.graph.cool/relay/v1/cjp4o1t9ln3kh0198j7x9kk84';
+const cache = new QueryResponseCache({size: 100, ttl: 100000});
 
-const network = Network.create((operation, variables) => {
+const network = Network.create((operation, variables, cacheConfig, uploadables) => {
+
+  const queryId = operation.name
+  const cachedData = cache.get(queryId, variables);
+
+  // Handle force option in RefetchOptions
+  // See: https://facebook.github.io/relay/docs/pagination-container.html
+  // https://facebook.github.io/relay/docs/refetch-container.html
+  const forceLoad = cacheConfig && cacheConfig.force
+
+  if (!forceLoad && cachedData) {
+    return cachedData;
+  }
+
+  if (forceLoad) {
+    // clear() means to reset all the cache, not only the entry addressed by specific queryId.
+    // See blog comments for more details.
+    cache.clear()
+  }
+
   return fetch(__RELAY_API_ENDPOINT__, {
     method: 'POST',
     headers: {
@@ -20,8 +41,10 @@ const network = Network.create((operation, variables) => {
       variables,
     }),
   }).then(response => {
-    return response.json()
-  })
+    const data = response.json();
+    // A cache key, queryId in this code, should be unique per query.
+    cache.set(queryId, variables, data);
+    return data  })
 })
 
 const environment = new Environment({
